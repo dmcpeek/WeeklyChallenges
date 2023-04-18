ea7875478a2c1cf0d89a2bbfa8141e8cfc99c621
Doug McPeek <dmcpeek@outlook.com>
2023-04-17 19:47:10 -0600

    Removed a lot of 'custom' code

--- ChallengesWithTestsMark8/ChallengesSet01.cs
+++ ChallengesWithTestsMark8/ChallengesSet01.cs
@@ -5,7 +5,7 @@ namespace ChallengesWithTestsMark8
 {
     public class ChallengesSet01
     {
-        public static bool AreTwoNumbersTheSame(int num1, int num2)
+        public bool AreTwoNumbersTheSame(int num1, int num2)
         {
             if(num1 == num2)
             {
@@ -17,20 +17,20 @@ namespace ChallengesWithTestsMark8
             }
         }
 
-        public static double Subtract(double minuend, double subtrahend)
+        public double Subtract(double minuend, double subtrahend)
         {
 
             return (minuend - subtrahend);
             //throw new NotImplementedException();
         }
 
-        public static int Add(int number1, int number2)
+        public int Add(int number1, int number2)
         {
             return (number1 + number2);
             //throw new NotImplementedException();
         }
 
-        public static int GetSmallestNumber(int number1, int number2)
+        public int GetSmallestNumber(int number1, int number2)
         {
             if (number1 < number2)
                 return number1;
@@ -39,21 +39,24 @@ namespace ChallengesWithTestsMark8
             //throw new NotImplementedException();
         }
 
-        public static long Multiply(long factor1, long factor2)
+        public long Multiply(long factor1, long factor2)
         {
             return (factor1 * factor2);
             //throw new NotImplementedException();
         }
 
-        public static string GetGreeting(string nameOfPerson)
+        public string GetGreeting(string nameOfPerson)
         {
-            return ($"Afternoon {nameOfPerson}. How's your family doin'?");
+            if (nameOfPerson == "")
+                return "Hello";
+            else
+                return ("Hello, {nameOfPerso}!");
             //throw new NotImplementedException();
         }
 
-        public static string GetHey()
+        public string GetHey()
         {
-            return ($"How many ways can you spell hey? Did you think of hay and hae?"); ;
+            return ("Hey!");
             //throw new NotImplementedException();
         }
     }
--- ChallengesWithTestsMark8/Program.cs
+++ ChallengesWithTestsMark8/Program.cs
@@ -6,34 +6,7 @@ namespace ChallengesWithTestsMark8
     {
         static void Main(string[] args)
         {
-            var booleanValue = ChallengesSet01.AreTwoNumbersTheSame(13, 13);
-            Console.WriteLine($"Are the two numbers the same? {booleanValue}");
-            Console.WriteLine();
 
-            double subtractValue;
-            subtractValue = ChallengesSet01.Subtract(12.9, 3.5);
-            Console.WriteLine($"The first number minus the second number equals {subtractValue}");
-            Console.WriteLine();
-
-            var addValue = ChallengesSet01.Add(99, 97);
-            Console.WriteLine($"The sum of the two numbers is {addValue}");
-            Console.WriteLine();
-
-            var smallestValue = ChallengesSet01.GetSmallestNumber(3, 5);
-            Console.WriteLine($"The smallest of the two numbers is {smallestValue}");
-            Console.WriteLine();
-
-            var multiplyValue = ChallengesSet01.Multiply(112358133144, 31415926);
-            Console.WriteLine("The product of the two numbers is " + multiplyValue.ToString("N0"));
-            Console.WriteLine();
-
-            var greetingValue = ChallengesSet01.GetGreeting("Bubba Jim Bob Jones");
-            Console.WriteLine(greetingValue);
-            Console.WriteLine();
-
-            var howdyValue = ChallengesSet01.GetHey();
-            Console.WriteLine(howdyValue);
-            Console.WriteLine();
         }
     }
 }
