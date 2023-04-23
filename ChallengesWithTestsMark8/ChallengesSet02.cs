8fc1b8686c1507c4f48b26f2b887820499d5e86c
Doug McPeek <dmcpeek@outlook.com>
2023-04-22 18:53:17 -0600

    Weekly challenge #2

--- ChallengesWithTestsMark8/ChallengesSet02.cs
+++ ChallengesWithTestsMark8/ChallengesSet02.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 
 namespace ChallengesWithTestsMark8
 {
@@ -7,52 +9,136 @@ namespace ChallengesWithTestsMark8
     {
         public bool CharacterIsALetter(char c)
         {
-            throw new NotImplementedException();
+            return (char.IsLetter(c));
+
+            //throw new NotImplementedException();
         }
 
         public bool CountOfElementsIsEven(string[] vals)
         {
-            throw new NotImplementedException();
+            return (vals.Length % 2 == 0);
+
+            //throw new NotImplementedException();
         }
 
         public bool IsNumberEven(int number)
         {
-            throw new NotImplementedException();
+            return (number % 2 == 0);
+
+            //throw new NotImplementedException();
         }
 
         public bool IsNumberOdd(int num)
         {
-            throw new NotImplementedException();
+            return (num % 2 != 0);
+            //throw new NotImplementedException();
         }
 
         public double SumOfMinAndMax(IEnumerable<double> numbers)
         {
-            throw new NotImplementedException();
+            // I had to look up a function to use with IEnumerable. I'd like to learn what this type is.
+            double min = double.MaxValue;
+            double max = double.MinValue;
+            
+            if (numbers == null || numbers.Any() == false)
+            {
+                return 0;
+            }
+            else 
+            {
+
+                foreach (var number in numbers)
+                {
+                    if (number < min)
+                    {
+                        min = number;
+                    }
+                    if (number > max)
+                    {
+                        max = number;
+                    }
+                        
+                }
+
+            }
+            return (min + max);
+            //throw new NotImplementedException();
         }
 
         public int GetLengthOfShortestString(string str1, string str2)
         {
-            throw new NotImplementedException();
+            var result = str1.Length <= str2.Length ? str1.Length : str2.Length;
+            
+            return result;
+
+            //throw new NotImplementedException();
         }
 
         public int Sum(int[] numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null)
+            { 
+                return 0; 
+            }
+
+            int sum = 0;
+           foreach (int number in numbers)
+           {
+                sum += number;
+           }
+            return sum;
+            
+            //throw new NotImplementedException();
         }
 
         public int SumEvens(int[] numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null)
+            {
+                return 0;
+            }
+
+            int sum = 0;
+            foreach (int number in numbers)
+            {
+                if(number % 2 == 0)
+                {
+                    sum += number;
+                }
+
+            }
+            return sum;
+            //throw new NotImplementedException();
         }
 
         public bool IsSumOdd(List<int> numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null)
+            { 
+                return false; 
+            }
+
+            int sum = 0;
+            foreach (int number in numbers)
+            {
+                sum += number;
+            }
+            
+            bool odd = (sum == 0 || sum % 2 == 0) ? false : true;
+            return odd;
+
+            //throw new NotImplementedException();
         }
 
         public long CountOfPositiveOddsBelowNumber(long number)
         {
-            throw new NotImplementedException();
+            long count = 0;
+            for (int i = 1; i < number; i+=2)
+            {
+                count++;
+            }
+            return count;
+            //throw new NotImplementedException();
         }
     }
 }
